95ebe3304f0d5851e74f03bc8e4ea4c21bfefcce
Milo Winningham <milo@winningham.net>
2012-11-17 22:19:22 -0800

    Strip whitespace

--- lib/colorscore/histogram.rb
+++ lib/colorscore/histogram.rb
@@ -4,17 +4,17 @@ module Colorscore
       output = `convert #{image_path} -resize 400x400 -format %c -dither None -quantize YIQ -colors #{colors} -depth #{depth} histogram:info:-`
       @lines = output.lines.sort.reverse.map(&:strip).reject(&:empty?)
     end
-    
+
     # Returns an array of colors in descending order of occurances.
     def colors
       hex_values = @lines.map { |line| line[/#([0-9A-F]{6}) /, 1] }.compact
       hex_values.map { |hex| Color::RGB.from_html(hex) }
     end
-    
+
     def color_counts
       @lines.map { |line| line.split(':')[0].to_i }
     end
-    
+
     def scores
       total = color_counts.inject(:+).to_f
       scores = color_counts.map { |count| count / total }
--- lib/colorscore/metrics.rb
+++ lib/colorscore/metrics.rb
@@ -3,19 +3,19 @@ module Colorscore
     def self.similarity(a, b)
       1 - distance(a, b)
     end
-    
+
     def self.distance(color_1, color_2)
       l1, a1, b1 = xyz_to_lab(*rgb_to_xyz(color_1))
       l2, a2, b2 = xyz_to_lab(*rgb_to_xyz(color_2))
-      
+
       distance = delta_e_cie_2000(l1, a1, b1, l2, a2, b2)
       scale(distance, 0..100)
     end
-    
+
     # Ported from colormath for Python.
     def self.delta_e_cie_2000(l1, a1, b1, l2, a2, b2)
       kl = kc = kh = 1
-      
+
       avg_lp = (l1 + l2) / 2.0
       c1 = Math.sqrt((a1 ** 2) + (b1 ** 2))
       c2 = Math.sqrt((a2 ** 2) + (b2 ** 2))
@@ -66,27 +66,27 @@ module Colorscore
 
       delta_e = Math.sqrt(((delta_lp / (s_l * kl)) ** 2) + ((delta_cp / (s_c * kc)) ** 2) + ((delta_hp / (s_h * kh)) ** 2) + r_t * (delta_cp / (s_c * kc)) * (delta_hp / (s_h * kh)))
     end
-    
+
     def self.rgb_to_xyz(color)
       color = color.to_rgb
       r, g, b = color.r, color.g, color.b
-      
+
       # assuming sRGB (D65)
       r = (r <= 0.04045) ? r/12.92 : ((r+0.055)/1.055) ** 2.4
       g = (g <= 0.04045) ? g/12.92 : ((g+0.055)/1.055) ** 2.4
       b = (b <= 0.04045) ? b/12.92 : ((b+0.055)/1.055) ** 2.4
-      
+
       r *= 100
       g *= 100
       b *= 100
-      
+
       x = 0.412453*r + 0.357580*g + 0.180423*b
       y = 0.212671*r + 0.715160*g + 0.072169*b
       z = 0.019334*r + 0.119193*g + 0.950227*b
-      
+
       [x, y, z]
     end
-    
+
     def self.xyz_to_lab(x, y, z)
       x /= 95.047
       y /= 100.000
@@ -97,13 +97,13 @@ module Colorscore
       else
         x = (7.787 * x) + (16.0 / 116)
       end
-      
+
       if y > 0.008856
         y = y ** (1.0/3)
       else
         y = (7.787 * y) + (16.0 / 116)
       end
-      
+
       if z > 0.008856
         z = z ** (1.0/3)
       else
@@ -116,7 +116,7 @@ module Colorscore
 
       [l, a, b]
     end
-    
+
     def self.scale(number, from_range, to_range=0..1, clamp=true)
       if clamp && number <= from_range.begin
         position = 0
@@ -125,10 +125,10 @@ module Colorscore
       else
         position = (number - from_range.begin).to_f / (from_range.end - from_range.begin)
       end
-      
+
       position * (to_range.end - to_range.begin) + to_range.begin
     end
-    
+
     def self.radians(degrees); degrees * Math::PI / 180; end
     def self.degrees(radians); radians * 180 / Math::PI; end
   end
